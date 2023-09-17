054f50ddca3b67ee56c297ad00095ef5db01f4a0
Asqarch1k <asqarnorqulov464@gmail.com>
2023-09-17 13:18:20 +0500

    A

--- js/table.js
+++ js/table.js
@@ -18,3 +18,20 @@ var swiper = new Swiper('.swiper', {
 
     return direction;
   }
+
+  // -------------------------------------------------
+
+  const acc = document.querySelector(".pluss");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
